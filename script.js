8ded485506b6b9d24c9b49768eeeb191e2ef6f12
Valeriereds <valeriereds@gmail.com>
2023-07-05 00:54:11 -0500

    Added psuedo code and start of js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Create needed variables for questions/answers
+var inTro = document.querySelectorAll(".intro-quiz");
+var startQuiz = document.querySelectorAll("#start-quiz");
+var quizHead = document.querySelectorAll("#quiz");
+
+// We will need event listeners for the question/answer transition
+
+var quesOne = document.querySelector("quesOne");
+
+inTro.textContent = "Coding Quiz Challenge";
+quizHead.textContent = "Prepared to burn your brain out into mush? Try your knowledge with this data coding quiz. Choose wisely! For every incorrect answer, the timer will deduct 15 seconds."
+
+
+
+
+
+// we will need set and clearInterval for timer
+// we're gonna need if/else statements for the evilistener
+// timer needs to decrease for every wrong answer, should these be apart of the if/else statements
