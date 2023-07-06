5911821eb2766e1c5e98cab54b82f1c7f21518a0
Valeriereds <valeriereds@gmail.com>
2023-07-05 23:53:33 -0500

    added psuedo coding and fixed js to link to html

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // Create needed variables for questions/answers
-var inTro = document.querySelectorAll(".intro-quiz");
-var startQuiz = document.querySelectorAll("#start-quiz");
-var quizHead = document.querySelectorAll("#quiz");
+// changed query selectorall to just selector because it functions towards all of them instead of just one
+// remember to reference the activities and mini project
+var inTro = document.querySelector(".intro");
+var startQuiz = document.querySelector(".start-button");
+var quizHead = document.querySelector(".quiz")
 
 // We will need event listeners for the question/answer transition
-
+// refer to event listener activities
 var quesOne = document.querySelector("quesOne");
 
-inTro.textContent = "Coding Quiz Challenge";
-quizHead.textContent = "Prepared to burn your brain out into mush? Try your knowledge with this data coding quiz. Choose wisely! For every incorrect answer, the timer will deduct 15 seconds."
+quizHead.textContent = "Coding Quiz Challenge";
+inTro.textContent = "Prepared to burn your brain out into mush? Try your knowledge with this data coding quiz. Choose wisely! For every incorrect answer, the timer will deduct 15 seconds."
+// add event listener to toggle from intro to first question
+function startQuest() {
+  isWin = false;
+  timerCount = 75;
+  // Prevents start button from being clicked when round is in progress
+  startButton.disabled = true;
+  startTimer()
+}
 
-
-
-
+// 
 
 // we will need set and clearInterval for timer
 // we're gonna need if/else statements for the evilistener
